5abcdfc902977c9115151e3aa2d780a4e1b3facd
asualosamuel4-ctrl <asualosamuel4@gmail.com>
2026-02-14 13:18:07 +0200

    first commit

---
+++ giant.js
@@ -0,0 +1,56 @@
+// Alert statement
+let user = "Samuel";
+age = 25;
+position = "student";
+height = "5.6";
+status = "single"
+
+alert(`my name is ${user},i am ${age} years old,i am a ${position},i am ${height} and ${status}`);
+
+// Alert statement
+user = "victor";
+age = "25";
+position = "student";
+height = "5.6 foot tall";
+status ="married lol";
+
+alert(`my name is ${user},i am ${age} years old,i am a ${position},i am ${height} and i am ${status}`);
+
+// Alert statement
+user = "jennifer";
+age = "25";
+position = "student";
+height = "5.4 foot tall";
+status = "single or married";
+
+alert(`my name is ${user} i am ${age} years old,i am a ${position},i am ${status} and i am ${height}`);
+
+// Alert statement
+user = "ezinne";
+age = "25"
+position = "student";
+height = "5.5 foot tall";
+status = "married";
+
+alert(`my name is ${user} i am ${age} years old,i am a ${position},i am ${status} and i am ${height}`);
+
+// Alert statement 
+user = "chiwendu";
+age = "25";
+position = "student in jenga tech";
+height = "5.8 foot tall";
+status = "married";
+
+alert(`my name is ${user},i am ${age} years old,i am a ${position},i am ${height} and i am ${status}`);
+
+// Alert statement 
+user = "mike";
+age = "25";
+status = "single";
+position = "student in jenga tech";
+height = "5.6"
+
+alert(`my name is ${user},i am ${age},i am a ${position},i am ${height} and i am ${status}`);
+
+
+
